100268b0702de1c5002f74df5f174a934292ca79
instantis <2400368muris@zuyd.nl>
2026-01-20 17:27:37 +0100

    Update Program.cs

--- restaurant/Program.cs
+++ restaurant/Program.cs
@@ -1,15 +1,16 @@
 using LeMacronnesResturauntAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Database Context toevoegen
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<RestaurantContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
-// 2. Controllers configureren
-// We voegen ReferenceHandler.IgnoreCycles toe om JSON loops te voorkomen bij testen zonder DTOs
+// 2. Controllers
 builder.Services.AddControllers().AddJsonOptions(x =>
    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
@@ -18,15 +19,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 3. Middleware Pipeline
-if (app.Environment.IsDevelopment())
+// 3. Forwarded headers voor HTTPS
+app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+// 4. Swagger altijd aan (optioneel)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
